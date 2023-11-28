80245cad9932b118d8085d6536fa7659770477c5
Murr <vtsmob@gmail.com>
2023-11-28 13:12:01 +0100

    added config

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
 
 namespace LightSwitcher
 {
+    public class AppConfig
+    {
+        public int ServerPort { get; set; }
+        public string StatusCommandEndpoint { get; set; }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                .Build();
+
+            var appConfig = configuration.Get<AppConfig>();
+
+            Console.WriteLine($"ServerPort: {appConfig.ServerPort}");
+            Console.WriteLine($"StatusCommandEndpoint: {appConfig.StatusCommandEndpoint}");
+
+            CreateHostBuilder(args, appConfig).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args, AppConfig appConfig) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls($"http://localhost:{appConfig.ServerPort}");
                 });
     }
 }
